bc8e5579cc3024674bb78f34ea613c6dec9cfed3
Zane Dayton <Zaneo.com@gmail.com>
2022-06-11 16:02:27 -0600

    more proving with friends

--- Week08/prove_with_friends.py
+++ Week08/prove_with_friends.py
@@ -1,6 +1,6 @@
 import random
 
-secret_word = ["red", "blue", "green", "yellow"]
+secret_word = ["red", "blue"]
 guess = ""
 count = 0
 length = 0
@@ -9,13 +9,17 @@ random_secret_word = random.choice(secret_word).lower()
 # print(f"random_secret_word: ", random_secret_word) # uncomment this on to see random word
 print()
 print("Welcome to the word guessing game!\n")
+
 print(f"Your hint is: ", end="")
 length_secret_word = len(random_secret_word)
 while length < length_secret_word:
     print(f"_", end=" ")
     length = length + 1
-guess = input("\nWhat is your guess? ").lower()
-while guess != random_secret_word:
+while True:
+    guess = input("\nWhat is your guess? ").lower()
+    count += 1
+    if guess == random_secret_word:
+        break
     print(f"Your hint is: ", end="")
     for j, letter in enumerate(guess):
         while letter in guess:
@@ -30,9 +34,6 @@ while guess != random_secret_word:
                 print(f"_ ", end="")
                 break
             break  
-    guess = input("\nWhat is your guess? ").lower()
-    count = count + 1
-count = count + 1
 print("Congratulations! You guessed it!")
 print(f"It took you {count} guesses.")
 print()
