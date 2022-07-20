a5eeddb529b2a61816ce0c37b9f08bd963a13b46
Zane Dayton <Day16006@byui.edu>
2022-07-19 23:53:50 -0600

    ok

--- Week13/windchill.py
+++ Week13/windchill.py
@@ -4,19 +4,15 @@ def convert_temp(value):
 def windchill_calc(T, mph):
     a_number = 35.74 + 0.6215 * T - 35.75 * (mph ** 0.16) + 0.4275 * T * (mph ** 0.16)
     return a_number
-
 number = 0
 mph = 5
 windchill = 0
 temp = 0
-
 value = int(input(f"What is the temperature? "))
 f_c = input(f"Fahrenheit or Celsius (F/C)? ").lower()
 if f_c == "c":
     value = convert_temp(value)
-
 while mph <= 60:
     windchill = windchill_calc(value, mph)
     print(f"At temperature {value:.1f}F, and wind speed {mph} mph, the windchill is: {windchill:.2f}F")
     mph += 5
-
